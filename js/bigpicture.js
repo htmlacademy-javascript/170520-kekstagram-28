f30670dcaecd0ffa1c26ff2e2d841bc0683fbd99
Anton Shchukin <a.a.shchukin@gmail.com>
2023-03-22 03:30:58 +0700

    Переорганизуем код так, чтобы обработчики закртия удалялись

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -8,11 +8,62 @@ const initBigPicture = () => {
   const $bigPictureCancel = document.querySelector('.big-picture__cancel');
   const bigPicturePreview = 'big-picture__preview'; /* Содержимое внутри модалки, но не сама модалка, понадобится внутри target.closest() */
 
-  /* Открытие */
+
+  /* Вспомогательные функции для того чтобы можно было повесить обработчики, СОХРАНИТЬ, а потом снять их */
+
+  const onBigPictureCrossClick = () => {
+    closeBigPicture();
+  }
+
+  const onBigPictureCrossKeydown = () => {
+    if (isEnterKey(event)) {
+      closeBigPicture();
+    }
+  }
+
+  const onDocumentKeydownToCloseBigPicture = (event) => {
+    if (isEscapeKey(event)) {
+      closeBigPicture();
+    }
+  }
+
+
+  /* Открытие модального окна */
 
   const openBigPicture = () => {
+
+    /* Показ */
     $bigPicture.classList.remove('hidden');
+
+
+    /* Обработчики закрытия */
+    $bigPictureCancel.addEventListener('click', onBigPictureCrossClick);
+    $bigPictureCancel.addEventListener('keydown', onBigPictureCrossKeydown);
+    document.addEventListener('keydown', onDocumentKeydownToCloseBigPicture);
+
+    $bigPicture.addEventListener('click', (event) => {
+      if (!event.target.closest('.big-picture__preview')) {
+        closeBigPicture();
+      }
+    });
+
   }
+
+
+  /* Закрытие модального окна */
+
+  const closeBigPicture = () => {
+    $bigPicture.classList.add('hidden');
+
+    /* Снятие обработчиков закрытия */
+    $bigPictureCancel.removeEventListener('click', onBigPictureCrossClick);
+    $bigPictureCancel.removeEventListener('keydown', onBigPictureCrossKeydown);
+    document.removeEventListener('keydown', onDocumentKeydownToCloseBigPicture);
+  }
+
+
+
+
 
   [].forEach.call($picturesList, function (element) {
     element.addEventListener('click', () => {
@@ -25,34 +76,6 @@ const initBigPicture = () => {
     })
   });
 
-
-  /* Закрытие */
-
-  const closeBigPicture = () => {
-    $bigPicture.classList.add('hidden');
-  }
-
-  $bigPictureCancel.addEventListener('click', () => {
-    closeBigPicture();
-  });
-
-  $bigPictureCancel.addEventListener('keydown', (event) => {
-    if( isEnterKey(event)) {
-      closeBigPicture();
-    }
-  });
-
-  $bigPicture.addEventListener('click', (event) => {
-    if( ! event.target.closest('.big-picture__preview') ) {
-      closeBigPicture();
-    }
-  });
-
-  document.addEventListener('keydown', (event) => {
-    if(isEscapeKey(event)) {
-      closeBigPicture();
-    }
-  });
 
 
 };
